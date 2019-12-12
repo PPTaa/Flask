2de20512e70b43c229cf0b1c428c2f7cb9488fbc
PPTaa <chobo05@naver.com>
2019-12-12 10:16:39 +0900

    route&def study

--- hello.py
+++ hello.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def hello():
     name = request.args.get("name", "World")
     return f'Hello, {escape(name)}!'
+
+@app.route('/hi')
+def hi():
+    return 'hi 안녕'
+
+@app.route('/pptaa')
+def pptaa():
+    return 'pptaa'
     
 if __name__=='__main__':
     app.run(debug=True)
