9e204c2fe4007b68c677d0095dd94bbcfbb901ff
PPTaa <chobo05@naver.com>
2019-12-12 10:42:18 +0900

    변수를 html파일로 넘겨주기

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template
 
 app = Flask(__name__)
 
@@ -14,6 +14,19 @@ def hi():
 @app.route('/pptaa')
 def pptaa():
     return 'pptaa'
+
+@app.route('/html_tag')
+def html_tag():
+    return '<h1>안녕하세용</h1>'
+
+@app.route('/html_file')
+def html_file():
+    return render_template('index.html')
+
+@app.route('/variable')
+def variable():
+    name = "이정철"
+    return render_template('variable.html', html_name=name)
     
 if __name__=='__main__':
     app.run(debug=True)
